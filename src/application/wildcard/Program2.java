cf7652d08c38eb835bbf6fce71a07b1fb0637af3
ricardo.roque <ricardo.roque@neomind.com.br>
2024-09-14 16:55:27 -0300

    Aula sobre Tipos Curingas e Tipos Curingas delimitados

---
+++ src/application/wildcard/Program2.java
@@ -0,0 +1,32 @@
+package application.wildcard;
+
+import entities.wildcard.Circle;
+import entities.wildcard.Rectangle;
+import entities.wildcard.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program2 {
+
+    public static void main(String[] args) {
+
+        List<Shape> myShapes = new ArrayList<>();
+        myShapes.add(new Rectangle(3.0, 2.0));
+        myShapes.add(new Circle(2.0));
+
+        List<Circle> myCircles = new ArrayList<>();
+        myCircles.add(new Circle(2.0));
+        myCircles.add(new Circle(3.0));
+
+        System.out.println("Total area: " + totalArea(myCircles));
+    }
+
+    public static double totalArea(List<? extends Shape> list) {
+        double sum = 0.0;
+        for (Shape s : list) {
+            sum += s.area();
+        }
+        return sum;
+    }
+}
