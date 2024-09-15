4d60afb0de97a0067adfdf65e2532b934580ea88
ricardo.roque <ricardo.roque@neomind.com.br>
2024-09-15 19:50:51 -0300

    Aula sobre equals e hashcode

---
+++ src/application/hashcode/Program.java
@@ -0,0 +1,12 @@
+package application.hashcode;
+
+public class Program {
+    public static void main(String[] args) {
+
+        String a = "Maria";
+        String b = "Alex";
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+    }
+}
